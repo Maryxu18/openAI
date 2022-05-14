a606f0657de1cfa071d6046a819168a796b3df5b
Maryxu18 <maryxyx14@gmail.com>
2022-05-13 21:53:00 -0400

    feat(form): add form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,57 @@
-function Form() {
+import React, { useState } from "react";
+import api from "../../services/api";
 
+function Form({ updateResponses }) {
+  const [prompt, setPrompt] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      prompt: prompt,
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+
+    if (prompt !== "") {
+      api
+        .post("/", data)
+        .then((res) => {
+          const newResponse = res.data.choices[0].text;
+          const newResponseDate = res.data.created;
+          updateResponses(prompt, newResponse, newResponseDate);
+        })
+        .catch((err) => {
+          //TODO: handle error
+        });
+    }
+  };
+  return (
+    <div className="mb-3">
+      <form name="responseForm" onSubmit={handleSubmit}>
+        <div class="mb-3">
+          <label className="form-label"> Enter a Prompt</label>{" "}
+          <textarea
+            value={prompt}
+            onChange={(event) => {
+              setPrompt(event.target.value);
+            }}
+            rows={5}
+            cols={30}
+            form="responseForm"
+            className="form-control"
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;

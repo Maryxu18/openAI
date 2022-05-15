8f5fae9e622a435dd9781a70f9c451e4d4d96f76
Maryxu18 <maryxyx14@gmail.com>
2022-05-14 20:47:08 -0400

    feat(redux): add reducer for addResponse action

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,19 @@
+const initialState = {
+  error: null,
+  responseList: []
+};
+
+const responseReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_RESPONSE_SUCCESS':
+      // state.responseList = [action.payload.response, ...state.responseList];
+      return { error: null, responseList: [action.payload, ...state.responseList] };
+    case 'ADD_RESPONSE_FAILURE':
+      // state.responseList = [action.payload.response, ...state.responseList];
+      return { ...state, error: action.payload.error };
+    default:
+      return state;
+  }
+};
+
+export default responseReducer;

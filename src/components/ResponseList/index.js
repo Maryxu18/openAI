b99c8fb8fa43a4ecbeebedc1aa9d81c555a51d75
Maryxu18 <maryxyx14@gmail.com>
2022-05-15 09:46:07 -0400

    feat(responseList): add pagination component in responseList

--- src/components/ResponseList/index.js
+++ src/components/ResponseList/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Response from '../Response';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Pagination from '../Pagination';
 
 function ResponseList({ responseList }) {
+  const pageSize = 2;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentResponses, setCurrentReponses] = useState(responseList.slice(0, pageSize));
+
+  const changePage = (page) => {
+    setCurrentPage(page);
+    setCurrentReponses(responseList.slice(page - 1, page + pageSize - 1));
+  };
+
   return (
     <div>
       <div className="h3" style={{ textAlign: 'left' }}>
@@ -16,6 +26,12 @@ function ResponseList({ responseList }) {
       ) : (
         <></>
       )}
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={responseList.length}
+        pageSize={pageSize}
+        onPageChange={(page) => changePage(page)}></Pagination>
     </div>
   );
 }

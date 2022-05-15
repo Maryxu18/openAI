e5be9e09dce491fb486a3afaf1c709dd00d04773
Maryxu18 <maryxyx14@gmail.com>
2022-05-14 21:40:13 -0400

    feat(redux): update index.js with redux store

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,23 +1,23 @@
 export const loadState = () => {
-    //localstorage getItem can fail if user privacy mode does not allow access
-    try {
-        const state = localStorage.getItem("responses");
-        if (state === null) {
-            return undefined
-        }
-        const responseList = JSON.parse(state)
-        return { responseList: responseList }
-        // return JSON.parse(state)
-    } catch (err) {
-        return undefined;
+  //localstorage getItem can fail if user privacy mode does not allow access
+  try {
+    const state = localStorage.getItem('responses');
+    if (state === null) {
+      return undefined;
     }
-}
+    const responseList = JSON.parse(state);
+    return { responseList: responseList };
+    // return JSON.parse(state)
+  } catch (err) {
+    return undefined;
+  }
+};
 
 export const saveState = (state) => {
-    try {
-        const stateString = JSON.stringify(state.responseList)
-        localStorage.setItem("responses", stateString);
-    } catch (err) {
-        //ignore write errors
-    }
-}
+  try {
+    const stateString = JSON.stringify(state.responseList);
+    localStorage.setItem('responses', stateString);
+  } catch (err) {
+    //ignore write errors
+  }
+};
